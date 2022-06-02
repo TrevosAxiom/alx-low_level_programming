5efb1c0a2f56912fdfeb1f882e6b8f504cd11f7e
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-02 07:36:18 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/* This program is to print out a string
-   Programming is fun BTW
-   Hard? Yes, but also FUN
+/*
+* This program is to print out a string
+* Programming is fun BTW
+* Hard? Yes, but also FUN
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
