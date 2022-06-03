8ad34423cedcfcac40a78117a0d906e0fd7af330
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-03 07:08:46 -0700

    Task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Main Declaration
+ * Return: 0
+ * Description: prints all single digit numbers of base 10
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+putchar('0' + digit);
+}
+
+putchar ('\n');
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c~
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Main Declaration
+ * Return: 0
+ * Description: prints all single digit numbers of base 10
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+printf("%d", digit);
+}
+
+putchar ('\n');
+
+return (0);
+}
