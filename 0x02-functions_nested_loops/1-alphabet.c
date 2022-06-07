11f0c68b1bc8ba771af4792ca3e622622d6a2d38
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-07 07:07:59 -0700

    Task 2... Feels like malaria

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - check the code
+*
+* Return: VOID.
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+_putchar (c);
+c++;
+}
+_putchar('\n');
+}
