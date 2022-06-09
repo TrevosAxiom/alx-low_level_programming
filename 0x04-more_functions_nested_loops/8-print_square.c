2a9a5fee27560c8e1f6469eb59c9906052af7ea5
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-09 13:20:07 -0700

    Chase equals, never chase the breadth

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_square - check for a digit
+* @n : number of _ to be printed
+* Return:void
+*/
+
+void print_square(int n)
+{
+
+int i = 0, num;
+
+while (i < n && n > 0)
+{
+num = 0;
+while (num < n)
+{
+    _putchar('#');
+    num++;
+}
+
+_putchar('\n');
+i++;
+}
+if (i == 0)
+_putchar('\n');
+
+}
