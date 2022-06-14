f1827cc08ed4035e6cf1d5db5bd0946681f2287b
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-14 11:43:33 -0700

    Need to Read on this well. NOTE TO SELF

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,11 +17,11 @@ sign *= -1;
 }
 }
 
- for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 {
 resp *= 10;
 resp += (s[i] - 48);
 }
 
-return (sign * resp);
+return (sign *resp);
 }
