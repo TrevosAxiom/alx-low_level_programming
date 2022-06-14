c71a6c63a57377fdac069116b31340ca9aa4157b
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-14 11:08:32 -0700

    Tongue Twister

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* rev_string - prints reversed string, followed by a new line
+* @s: pointer to the string to print
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+
+int length, i, div;
+char temp;
+
+for (length = 0; s[length] != '\0'; length++);
+
+i = 0;
+
+div = length / 2;
+
+while (div--)
+{
+temp = s[length - i - 1];
+s[length - i - 1] = s[i];
+s[i] = temp;
+i++;
+}
+
+}
