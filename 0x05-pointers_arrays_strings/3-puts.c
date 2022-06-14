a4fc53e3a41b0a5269901f5f07e563c4f30bced6
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-14 10:54:50 -0700

    Stringzzzz

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _puts - prints a string, followed by a new line,
+* @str: pointer to the string to print
+* Return: void
+*/
+
+
+void _puts(char *str)
+{
+int num = 0;
+while (str[num])
+{
+_putchar(str[num]);
+num++;
+}
+_putchar('\n');
+}
