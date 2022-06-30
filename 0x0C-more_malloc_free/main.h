cd60a45b490d135616a9a0ec34afdd022120909c
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-30 13:23:44 -0700

    ALX this is getting too hard man

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,6 @@ char *_memset(char *s, char b, unsigned int n);
 void *malloc_checked(unsigned int b);
 unsigned int _strlen(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-
+int *array_range(int min, int max);
 
 #endif
