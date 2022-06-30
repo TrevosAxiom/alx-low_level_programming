3927ea2d509cd64d242102c61c6e7539afcd88de
Adebayo Taofik <thetaoofwp@gmail.com>
2022-06-30 13:19:59 -0700

    I might be loosing my mind here

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
 void *malloc_checked(unsigned int b);
 unsigned int _strlen(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
