2b9635e2b56f190b656942e3a95fe38ad4d0060d
Adebayo Taofik <thetaoofwp@gmail.com>
2022-07-04 14:01:57 -0700

    Man like Mario

---
+++ pld/betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Main Entrance
+ * Return: 0 because it's a void
+ */
+int main(void)
+{
+int num;
+while (num <= 10)
+{
+printf("%d, ", num);
+num++;
+}
+putchar('\n');
+return (0);
+
+}
